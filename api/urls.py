c7724418a1594b5629f3635ebb0a93ebf3465415
Sanmi-Owoka <owokaayoola@outlook.com>
2022-04-28 08:45:07 +0100

    updated urls

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("subscription/get", GetSubscriptionView.as_view(), name='get subscription'),
     path("subscription/update/<int:pk>", UpdateSubscriptionView.as_view(), name='update subscription'),
     path("session/create", CreateSessionView.as_view(), name='create session'),
-    path("generateLink", GenerateMeetingLink.as_view(), name='generate link'),
-    path("session/confirm/<int:pk>", ConfirmSession.as_view(), name='confirm session')
+    path("session/generateMeeetingLink", GenerateMeetingLink.as_view(), name='generate link'),
+    path("session/confirm/<int:pk>", ConfirmSession.as_view(), name='confirm session'),
 ]
